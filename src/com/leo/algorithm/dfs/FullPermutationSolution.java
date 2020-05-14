bca13edee9f7b3d302927e10830565a7b70d894d
leo.chen <233346888@qq.com>
2020-05-15 00:10:48 +0800

    formatted in idea

--- src/com/leo/algorithm/dfs/FullPermutationSolution.java
+++ src/com/leo/algorithm/dfs/FullPermutationSolution.java
@@ -5,14 +5,11 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-public class FullPermutationSolution
-{
-    public static List<List<Integer>> permute(int[] nums)
-    {
+public class FullPermutationSolution {
+    public static List<List<Integer>> permute(int[] nums) {
         int len = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        if (len == 0)
-        {
+        if (len == 0) {
             return res;
         }
 
@@ -24,17 +21,13 @@ public class FullPermutationSolution
     }
 
     private static void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path,
-            List<List<Integer>> res)
-    {
-        if (depth == len)
-        {
+                            List<List<Integer>> res) {
+        if (depth == len) {
             res.add(new ArrayList<>(path));
             return;
         }
-        for (int i = 0; i < len; i++)
-        {
-            if (used[i])
-            {
+        for (int i = 0; i < len; i++) {
+            if (used[i]) {
                 continue;
             }
             path.addLast(nums[i]);
@@ -46,13 +39,11 @@ public class FullPermutationSolution
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] nums =
-        { 1, 2, 3 };
+                {1, 2, 3};
         List<List<Integer>> res = permute(nums);
-        for (List<Integer> l : res)
-        {
+        for (List<Integer> l : res) {
             System.out.println(l.toString());
 
         }
